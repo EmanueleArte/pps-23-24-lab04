7b0858348bd4d6b14db3eba6cdb2825fab17f3a0
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-03-25 11:27:18 +0100

    Completed ex1

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -11,7 +11,9 @@ object Ex1ComplexNumbers:
 
   trait ComplexADT:
     type Complex
+
     def complex(re: Double, im: Double): Complex
+
     extension (complex: Complex)
       def re(): Double
       def im(): Double
@@ -19,14 +21,22 @@ object Ex1ComplexNumbers:
       def subtract(other: Complex): Complex
       def asString(): String
 
+  case class ComplexNumber(re: Double, im: Double)
+
   object BasicComplexADT extends ComplexADT:
 
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    type Complex = ComplexNumber
+
+    def complex(re: Double, im: Double): Complex = ComplexNumber(re, im)
+
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex.re
+      def im(): Double = complex.im
+      def sum(other: Complex): Complex = ComplexNumber(complex.re() + other.re(), complex.im() + other.im())
+      def subtract(other: Complex): Complex = ComplexNumber(complex.re() - other.re(), complex.im() - other.im())
+      def asString(): String = complex match
+        case ComplexNumber(re, im) if re == 0 && im == 0 => "0.0"
+        case ComplexNumber(re, im) if re == 0 => s"${im}i"
+        case ComplexNumber(re, im) if im == 0 => s"$re"
+        case ComplexNumber(re, im) => s"$re ${if im < 0 then "- " + (-im) else "+ " + im}i"
