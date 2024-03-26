5b46e3390d906fa1f9ffad31c2fdf781a380fd03
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-03-26 18:50:02 +0100

    Completed ex5

--- src/main/scala/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,9 @@
 package u04lab
-import u03.Sequences.* 
+
+import u03.Sequences.*
 import Sequence.*
+import u03.Optionals.*
+import Optional.*
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -17,10 +20,31 @@ import Sequence.*
 
 object Ex5Traversable:
 
-  def log[A](a: A): Unit = println("The next element is: "+a)
+  def log[A](a: A): Unit = println("The next element is: " + a)
 
-  def logAll[A](seq: Sequence[A]): Unit = seq match
-    case Cons(h, t) => log(h); logAll(t)
-    case _ => ()
+  def logAll[T[_]: Traversable, A](seq: T[A])(log: A => Unit): Unit =
+    summon[Traversable[T]].foreach(seq)(log)
 
+  trait Traversable[T[_]]:
+    def foreach[A](seq: T[A])(f: A => Unit): Unit
+
+  given Traversable[Sequence] with
+    def foreach[A](seq: Sequence[A])(f: A => Unit): Unit = seq match
+      case Cons(h, t) => f(h); foreach(t)(f)
+      case _ => ()
+
+  given Traversable[Optional] with
+    def foreach[A](opt: Optional[A])(f: A => Unit): Unit = opt match
+      case Just(a) => f(a)
+      case _ => ()
+
+  @main def tryLogAll: Unit =
+
+    logAll[Sequence, Int](Cons(10, Cons(20, Cons(30, Nil()))))(log)
+    logAll[Optional, Int](Just(1))(log)
+    logAll[Optional, Int](Empty())(log)
+
+    logAll[Sequence, Int](Cons(10, Cons(20, Cons(30, Nil()))))(println(_))
+    logAll[Optional, Int](Just(1))(println(_))
+    logAll[Optional, Int](Empty())(println(_))
   
